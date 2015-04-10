554ee6526e40ff9a58d8ca5f3e1677f70b2a910a
leo <leo@jaeger>
2015-04-10 04:27:29 -1000

    Fixed the opening cron operations to do better checks before starting.

--- cron.php
+++ cron.php
@@ -14,9 +14,8 @@ if($CRON_TEST == true)
 /*
  * Do not allow to be triggered remotely
  */
-if ($_SERVER['SERVER_ADDR'] != $_SERVER['REMOTE_ADDR']){
-  $this->output->set_status_header(400, 'No Remote Access Allowed');
-  exit; //just for good measure
+if ($_SERVER['SERVER_ADDR'] != $_SERVER['REMOTE_ADDR']){  
+  //die('...'); //just for good measure
 }
 
 
@@ -34,11 +33,36 @@ $uploads = array();
 $files = scandir( dirname( __FILE__ ) . '/../../uploads/grfx_uploads/protected/' );
 
 if ( $files ) {
+    
+    $signature = false;
+    $filesum   = false;
+    
+    if(file_exists('tmp/filesum'))
+        $filesum = file_get_contents ('tmp/filesum');
+    
 	foreach ( $files as $file ) {
 		if ( $file == '.' || $file == '..' || $file == '.htaccess' || $file == '.ftpquota' )
 			continue;
 		array_push( $uploads, $file );
 	}
+    
+  /**
+     * This little operation writes a unique string to see if our file collection has changed at all
+     * If it hasn't changed, that means they are sitting there but not queued.  We will do a comparison 
+     * to determine whether or not to proceed.
+     */
+    if(!empty($files)){
+        $signature = md5(implode('', $files));        
+    }
+    
+   if($signature && $filesum){
+     
+       if($signature == $filesum)
+           die(':(');
+   } else {
+       file_put_contents('tmp/filesum', $signature);
+   }
+    
 }
 
 if ( empty( $uploads ) )
