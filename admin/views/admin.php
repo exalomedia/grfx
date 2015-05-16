20e8a8217b4f137901a6ecf80602803cab331a23
orangeman555 <leo@grfx.co>
2015-05-15 15:35:55 -1000

    Updated

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -50,8 +50,7 @@
         
         <!-- STEP 4 -->
         
-        <li>
-            <hr />
+        <li>            
             <p>
             <?php
             _e('Need help? Join the community at <strong><a href="http://community.grfx.co/">community.grfx.co/</a></strong>. We want your image-selling
@@ -61,5 +60,6 @@
         </li>
         
 	</ol>
-	
+	    
 </div>
+
